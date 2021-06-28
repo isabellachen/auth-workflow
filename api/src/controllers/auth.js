47eeddb3f026fa5863643cc0632be38c4ad5f37f
Isabella Chen <miss.izzyc@gmail.com>
2021-06-28 13:34:29 +0800

    update send() to json()

--- api/src/controllers/auth.js
+++ api/src/controllers/auth.js
@@ -17,13 +17,13 @@ export async function signUp(req, res) {
   const { name, email, password } = req.body;
 
   if (!name || !email || !password) {
-    res.status(400).send(`You're missing a property`);
+    res.status(400).json(`You're missing a property`);
   }
 
   const user = await User.findOne({ email });
 
   if (user) {
-    res.status(400).send('User already exists');
+    res.status(400).json('User already exists');
     return;
   }
 
@@ -37,7 +37,7 @@ export async function signUp(req, res) {
 
   const token = createToken(newUserInfo);
 
-  res.status(200).send({
+  res.status(200).json({
     name: newUserInfo.name,
     email: newUserInfo.email,
     access_token: token
@@ -47,28 +47,29 @@ export async function signUp(req, res) {
 export async function signIn(req, res) {
   const { email, password } = req.body;
   if (!email || !password) {
-    res.status(400).send('Username and password must be provided');
+    res.status(400).json('Username and password must be provided');
     return;
   }
   const user = await User.findOne({ email });
 
   if (!user) {
-    res.status(400).send('User not found');
+    res.status(400).json('User not found');
     return;
   }
 
   const validPassword = bcrypt.compare(password, user.password);
   if (!validPassword) {
-    res.status(400).send('Wrong password');
+    res.status(400).json('Wrong password');
     return;
   }
   const token = createToken(user);
   console.log(user);
   res
     .status(200)
-    .send({ name: user.name, email: user.email, access_token: token });
+    .json({ name: user.name, email: user.email, access_token: token });
 }
 
 export function profile(req, res) {
-  res.json('Resource only for authenticated users');
+  res.status(200).json('Resource only for authenticated users');
+  // res.json('Resource only for authenticated users');
 }
